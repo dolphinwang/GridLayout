0ca9a6db689b1ac4b1fa67c0465c4e9e3d791718
dolphinwang <dolphinwang@foxmail.com>
2014-05-15 17:35:38 +0800

    Demo

--- gen/com/dolphin/gridlayout/R.java
+++ gen/com/dolphin/gridlayout/R.java
@@ -93,6 +93,12 @@ containing a value of this type.
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int image=0x7f020001;
+        public static final int img1=0x7f020002;
+        public static final int img2=0x7f020003;
+        public static final int img3=0x7f020004;
+        public static final int img4=0x7f020005;
+        public static final int img5=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
